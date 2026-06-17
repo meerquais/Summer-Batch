20db9f4e25c62ebe392bb87ed1fc8bab8dff918b
Meer Quais <meerquais017@gmail.com>
2026-06-17 17:53:53 +0500

    Variables and data types

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,124 @@
+console.log("JavaScript Connected!");
+
+// syntax
+
+
+
+var student = "Ayan";
+
+// var keyword || var
+// variable name || student
+// assigning operator || = 
+// data  || 1,2,3 , "Ayan"
+
+
+console.log(student);
+
+
+// declaration     // initalization
+// var username        = "meer";
+
+
+// var userdata = 10;
+
+// re-initalization
+
+// student = "Hunain";
+
+// console.log(student);
+
+// Illegal JavaScript Variable names
+
+// javaScript keywords : if / for / class / switch / var
+
+// var if = "20"; X 
+
+// No special chars except $ (dollar sign) and _ (underscore) 
+
+// var %name = "meer"; 
+
+// Cannot user numbers before.
+
+// var 1student = "minhaj"; 
+
+// No space between names
+
+
+// legal variable names!
+
+// var $student = "Hafeez";
+
+// console.log($student);
+
+// var student1 = "Gul";
+
+// console.log(student1);
+
+// Camel case
+// Snake case
+// Pascal case
+
+
+// camel case
+
+// var user = "Minhaj";
+// var userData = "Meer";
+// var userEmailData;
+
+// snake case
+
+// var user; 
+// var user_data;
+// var user_email_data;
+
+// pascal 
+
+// var User;
+// var UserData;
+// var UserEmailUser;
+
+
+// primitives data type
+
+// String ===> " "
+// Number ===> 1 2 3 4 5 6 7 8 9 10
+// Boolean ===> true or false
+// undefined ===> undefined
+// null ===> null 
+
+// String
+
+var username = "Meer"; // ====> String Data Type. 
+
+console.log(username);
+
+// Number
+
+var userAge = 25; // ====> Number Data Type.
+
+console.log(userAge); 
+
+// Boolean
+
+var isVerified = true; // ====> Boolean Data Type.
+var isNew = false; // ====> Boolean Data Type.
+
+console.log(isVerified);
+console.log(isNew);
+
+// undefined.
+
+var isAvailabe = undefined; // ====> undefined Data type.
+var isPresent;  // ====> undefined Data type.
+
+console.log(isAvailabe);
+console.log(isPresent);
+
+// null
+
+var userData = null; // ====> null Data Type.
+
+console.log(userData);
+
+
+
