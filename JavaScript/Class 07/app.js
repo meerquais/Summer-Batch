54e7ae37ddaf3ad9d32a73a2c7488bcf31f80c86
Meer Quais <meerquais018@gmail.com>
2026-07-10 19:53:13 +0500

    Function

---
+++ JavaScript/Class 07/app.js
@@ -0,0 +1,61 @@
+console.log("JavaScript Connected");
+
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+
+// var i = 1;
+
+// while (i <= 10) {
+//     console.log(i);
+//     i++
+// }
+
+
+// for (var i = 1; i <= 5; i++) {
+
+//     console.log(i, "Outer Loop");
+
+
+//     for (var j = 1; j <= 5; j++) {
+//         console.log(j, "inner loop");
+//     }
+// }
+
+
+// var name = "Minhaj"
+// var name1 = "Hafez"
+
+// console.log("Welcome " + name);
+// console.log("Welcome " + name1);
+
+
+// function greetUser() {
+//     console.log("Welcome User");
+// }
+
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
+
+
+// parameters
+// arguments
+
+
+var userName = prompt("Type your name");
+
+function greet(user) {
+    console.log("Welcome " + user);
+};
+
+
+greet(userName)
+
+// greet("Minhaj")
+// greet("Hafeez")
+// greet("Ali")
